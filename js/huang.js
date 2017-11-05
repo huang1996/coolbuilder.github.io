0b12c2aeb2fe29fd622b67b9b3ee4b90fc38556c
huang1996 <676564115@qq.com>
2017-11-05 13:23:21 +0800

    1
    1

--- js/huang.js
+++ js/huang.js
@@ -15,20 +15,34 @@ window.onload=function () {
         frames:{width:100,height:100,regX:10,regY:10}
     }
     sprite  = new createjs.Sprite(new createjs.SpriteSheet(data));
-    createjs.Ticker.setFPS(10);
+    createjs.Ticker.setFPS(15);
     createjs.Ticker.addEventListener("tick",tick);
 
 }
 function tick(e) {
-    var s=sprite.clone();
-    s.x=Math.random()*1800;
-    s.y=Math.random()*1000;
-    s.scaleX=s.scaleY=Math.random();
-    s.alpha=Math.random();
-    stage.addChild(s);
-    if (stage.getNumChildren()>50)
+    var num=stage.getNumChildren();
+    if (num<100)
     {
-        stage.removeAllChildren();
+        var s=sprite.clone();
+        s.x=Math.random()*1800;
+        s.y=Math.random()*1000;
+        s.scaleX=s.scaleY=Math.random();
+        s.alpha=Math.random();
+        stage.addChild(s);
+    }
+    for (var i=num-1;i>=0;i--)
+    {
+        var s=stage.getChildAt(i);
+        if (s.x>1800 || s.y>1000)
+        {
+            stage.removeChildAt(i);
+        }
+        else
+        {
+            s.x+=Math.random()*2;
+            s.y+=Math.random()*1;
+            s.alpha=Math.random();
+        }
     }
     stage.update(e);
 
